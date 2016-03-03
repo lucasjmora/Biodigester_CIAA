a15efc8198e9a5f05c9184e391d329a47386729b
Lucas J. Mora <lucasjmora@gmail.com>
2016-03-03 02:26:10 -0300

    Cuasi-module dio_relay
    Implementation cuasi-module dio_relay for a more direct relays
    operations and status check
    
    API
    * int8_t ciaaDIO_relay_op(int32_t fildes_out, uint8_t relay_id, bool oper)
    * int8_t ciaaDIO_relay_st(int32_t fildes_out, uint8_t realy_id)
    
    NOTE: dirty code and comments. It must be solve in the future

---
+++ inc/dio_relay.h
@@ -0,0 +1,109 @@
+/* Copyright 2014, ACSE & CADIEEL
+ *    ACSE   : http://www.sase.com.ar/asociacion-civil-sistemas-embebidos/ciaa/
+ *    CADIEEL: http://www.cadieel.org.ar
+ * All rights reserved.
+ *
+ *    or
+ *
+ * Copyright 2014, Your Name <youremail@domain.com>
+ * All rights reserved.
+ *
+ *    or
+ *
+ * Copyright 2014, ACSE & CADIEEL & Your Name <youremail@domain.com
+ *    ACSE   : http://www.sase.com.ar/asociacion-civil-sistemas-embebidos/ciaa/
+ *    CADIEEL: http://www.cadieel.org.ar
+ * All rights reserved.
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef DIO_RELAY_H
+#define DIO_RELAY_H
+/** \brief Short description of this file
+ **
+ ** Long description of this file
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup Template Template to start a new module
+ ** @{ */
+
+/*==================[inclusions]=============================================*/
+#include "ciaaPOSIX_stdio.h"
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================[macros]=================================================*/
+#define  ON    true
+#define  OFF   false
+#define  ERR   -1
+#if (ciaa_nxp == BOARD)
+#define RELAY_1   4  
+#define RELAY_2   5  
+#define RELAY_3   6  
+#define RELAY_4   7  
+#elif (edu_ciaa_nxp == BOARD)
+/* edu_ciaa section */
+#define RELAY_1   3  /* LED 1 on edu-ciaa-nxp */  
+#define RELAY_2   4  /* LED 2 on edu-ciaa-nxp */  
+#define RELAY_3   5  /* LED 3 on edu-ciaa-nxp */  
+#define RELAY_4   7  /* LED RED on edu-ciaa-nxp */  
+#endif
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+/** \brief Adds 2 16 bits signed and return a 16 bits signed value
+ **
+ ** If the result is bigger than the limit in 15 bits returns
+ ** INT16_MAX is lower than the limit in 15 bits returns
+ ** INT16_MIN
+ **
+ ** \param[in]    a first parameter to be added
+ ** \param[in]    b second parameter ot be added
+ ** \return E_OK  returns addition of a+b
+ **/
+extern int8_t ciaaDIO_relay_op(int32_t fildes_out, uint8_t relay_id, bool oper);
+extern int8_t ciaaDIO_relay_st(int32_t fildes_out, uint8_t realy_id);
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+}
+#endif
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef DIO_RELAY_H */
+
